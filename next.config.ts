928953bff8c987adbcb9cae48fdd6d0c98b24365
Sudhir Kaushik <sudhirkaushk@outlook.com>
2025-02-01 01:46:40 +0530

    another build test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
   sassOptions: {
     additionalData: `$var: red;`,
   },
