987429fc6342843023fd5cf369bf36d3efef24eb
Sudhir Kaushik <sudhirkaushk@outlook.com>
2025-02-19 15:09:02 +0530

    build fix test 5

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,6 @@ const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `$var: red;`,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Add the fallback for fs
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        "aws-sdk": false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
