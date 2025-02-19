258a7d56103afee81777708ec4d6c7570b1f712e
Sudhir Kaushik <sudhirkaushk@outlook.com>
2025-02-19 15:00:34 +0530

    build fix test 2

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `$var: red;`,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Add the fallback for fs
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
